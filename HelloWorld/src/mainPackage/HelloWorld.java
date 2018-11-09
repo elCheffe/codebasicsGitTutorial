dc7ba3c00b2d785e929a92b7efb36205e70ab379
elCheffe <26125650+elCheffe@users.noreply.github.com>
2018-11-09 15:54:14 +0200

    added hungry prompt

--- HelloWorld/src/mainPackage/HelloWorld.java
+++ HelloWorld/src/mainPackage/HelloWorld.java
@@ -1,10 +1,26 @@
 package mainPackage;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, world!");
+		
+		System.out.println("Are you hungry? (yes, no)");
+		
+		String hungry = "";
+		
+		Scanner scanner = new Scanner(System.in);
+		hungry = scanner.nextLine();
+		
+		if (hungry.toLowerCase().equals("yes")) {
+			System.out.println("eat samosa! :)");
+		} else {
+			System.out.println("do your homework.");
+		}
+		
 	}
 
 }
